939ecb359c22ad751962ab28607af72c55808d0d
GabyCastt <gabykim928@gmail.com>
2024-07-07 10:58:00 -0600

    W

--- Ej2/views/html/productos.php
+++ Ej2/views/html/productos.php
@@ -65,7 +65,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Insertar o Actualizar Producto</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Insertar/Actualizar Producto</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
